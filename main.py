64f10dd9cdbd97e3180777c10061bb4f0201d798
Nesta Parchment <saint.clever.np@gmail.com>
2021-02-08 19:50:33 -0500

    edit show_data method

--- main.py
+++ main.py
@@ -2,73 +2,88 @@
 from kivymd.app import MDApp
 from kivymd.theming import ThemeManager
 from kivy.uix.screenmanager import ScreenManager, Screen
-
+from kivy.core.window import Window
+Window.size = (400, 700)
 
 # widgets
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.chip import MDChip
-from kivy.core.image import Image
+from kivymd.uix.list import ThreeLineAvatarListItem, MDList, IconLeftWidget
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.label import MDLabel
 
 
 # properties
-from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 
 
 # request
 import requests
 
 
-# gitHub Jobs json
-url = 'https://jobs.github.com/positions'
-json_data = 'https://jobs.github.com/positions.json'
-response = requests.get(url=json_data)
-response.raise_for_status()
-data = response.json()
-
-# print(
-#     data['type'],
-#     data['url'],
-#     data['company'],
-#     data['company_url'],
-#     data['location'],
-#     data['title'],
-#     data['description']
-# )
-
 
 
 class MainMenuScreen(Screen):
-    job_desc = ObjectProperty(None)
-    job_locate = ObjectProperty(None)
 
-    def show_data(self, *args):
-        print(f'https://jobs.github.com/positions.json?description={self.job_desc.text}&location={self.job_locate.text}')
-
-
-
-
-class JobListingsScreen(Screen):
-    def display_list(self):
-        job = self.manager.get_screen('main_menu').job_desc.text
-        loc = self.manager.get_screen('main_menu').job_locate.text
+    def show_data(self):
+        job_desc = ObjectProperty(None)
+        job_locate = ObjectProperty(None)
         
-        print(job, loc)
+        job_location = [self.job_desc.text, self.job_locate.text]     
+        # print(self.job_location)
+        
+        return job_location
 
 
 
+class JobListingsScreen(Screen):        
+
+    def display_list(self):
+        self.des_n_loc = MainMenuScreen()
+        # print(des_n_loc.show_data())
+        description = self.des_n_loc.show_data()[0]
+        location = self.des_n_loc.show_data()[1]
+
+        description = 'python'
+        location = 'new york'
+        
+        json_data = f'https://jobs.github.com/positions.json?description={description}&location={location}'
+        # print(json_data)
+
+        response = requests.get(url=json_data)
+        response.raise_for_status()
+        data = response.json()
+    
+        scroll = ScrollView() 
+        md_list = MDList()
+        scroll.add_widget(md_list)
+        
+        
+        for i in range(0, len(data)):
+            company_logo = data[i]['company_logo']
+            company = data[i]['company']
+            title = data[i]['title']
+            location = data[i]['location']
+            
+            icon_left_widget = IconLeftWidget(icon=f'language-{description}')
+            list_item = ThreeLineAvatarListItem(
+                text=company,
+                secondary_text=title,
+                tertiary_text=location,
+            )
+            
+            list_item.add_widget(icon_left_widget)
+            md_list.add_widget(list_item)
+        self.add_widget(scroll)
+
+        
 
 class GitHubJobsApp(MDApp):
     def build(self):
-        self.theme_cls.primary_palette = 'Orange'
+        self.theme_cls.primary_palette = 'DeepOrange'
         self.theme_cls.primary_hue = '500' # 200
         self.theme_cls.theme_style = 'Dark'  # 'Light'
-        
-        screen_manager = ScreenManager()
-        screen_manager.add_widget(MainMenuScreen(name='main_menu'))
-        screen_manager.add_widget(JobListingsScreen(name='job_listings'))
-        return screen_manager
-
         
 GitHubJobsApp().run()
--- main_00.py
+++ main_00.py
@@ -1,8 +1,9 @@
 # app and screen manager
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.theming import ThemeManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+Window.size = (400, 700)
 
 # widgets
 from kivymd.uix.tab import MDTabsBase
@@ -10,23 +11,25 @@ from kivymd.uix.textfield import MDTextField
 from kivymd.uix.button import MDRectangleFlatButton
 from kivymd.uix.chip import MDChip
 from kivy.core.image import Image
+from kivymd.uix.list import ThreeLineAvatarListItem, MDList, IconLeftWidget
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.label import MDLabel
+
 
 # properties
 from kivy.properties import ObjectProperty
+
 
 # request
 import requests
 
 
-kivy_file = Builder.load_file('githubjobs.kv')
-
-# gitHub Jobs
+# gitHub Jobs json
 url = 'https://jobs.github.com/positions'
 json_data = 'https://jobs.github.com/positions.json'
 response = requests.get(url=json_data)
 response.raise_for_status()
 data = response.json()
-
 
 # print(
 #     data['type'],
@@ -41,33 +44,83 @@ data = response.json()
 
 
 
+class MainMenuScreen(Screen):
 
-class MenuScreen(Screen):
-    job_desc = ObjectProperty(None)
-    job_locate = ObjectProperty(None)
-    pass
-    # def show_data(self):
-    #     # print(self.job_desc.text)
-    #     # print(self.job_locate.text)
-    #     print(f'https://jobs.github.com/positions.json?description={self.job_desc.text}&location={self.job_locate.text}')
+    def show_data(self):
+        job_desc = ObjectProperty(None)
+        job_locate =  ObjectProperty(None)
+        
+        self.job_location = [self.job_desc.text, self.job_locate.text]
+        # print(job_location)
 
-
-class ProfileScreen(Screen):
-    pass
-
-
-class WindowManager(ScreenManager):
-    pass
+        return self.job_location
 
 
 
-class GitHubJobsApp(MDApp):
-           
-    def build(self):
-        # self.theme_cls.primary_palette = 'Blue'
-        # self.theme_cls.primary_hue = '500' # 200
-        # self.theme_cls.theme_style = 'Light'  # 'Light'
-        return kivy_file
+class JobListingsScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        des_n_loc = MainMenuScreen()
+        print(des_n_loc.show_data())
+        description = des_n_loc.show_data()[0]
+        location = des_n_loc.show_data()[1]
 
         
+        json_data = f'https://jobs.github.com/positions.json?description={description}&location={location}'
+        print(json_data)
+
+        response = requests.get(url=json_data)
+        response.raise_for_status()
+        data = response.json()
+    
+        md_list = MDList()
+        scroll = ScrollView() 
+        scroll.add_widget(md_list)
+        
+        
+        for i in range(0, len(data)):
+            
+            company_logo = data[i]['company_logo']
+            company = data[i]['company']
+            title = data[i]['title']
+            location = data[i]['location']
+            
+            icon_left_widget = IconLeftWidget(icon=f'language-{description}')
+            
+            list_item = ThreeLineAvatarListItem(
+                text=company,
+                secondary_text=title,
+                tertiary_text=location,
+            )
+            
+            list_item.add_widget(icon_left_widget)
+            md_list.add_widget(list_item)
+            
+        self.add_widget(scroll)
+                
+
+
+    # def display_list(self):
+    #     self.job = self.manager.get_screen('main_menu').job_desc.text
+    #     self.loc = self.manager.get_screen('main_menu').job_locate.text
+    #     print(self.job, self.loc)
+        
+    #     self.json_data = f'https://jobs.github.com/positions.json?description={self.job}&location={self.loc}'
+    
+    
+
+class GitHubJobsApp(MDApp):
+    def build(self):
+        
+        self.theme_cls.primary_palette = 'Blue'
+        self.theme_cls.primary_hue = '500' # 200
+        self.theme_cls.theme_style = 'Light'  # 'Light'
+        
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(MainMenuScreen(name='main_menu'))
+        screen_manager.add_widget(JobListingsScreen(name='job_listings'))
+        
+        return screen_manager
+
 GitHubJobsApp().run()
